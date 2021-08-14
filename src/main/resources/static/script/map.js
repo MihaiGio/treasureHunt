6bcb82bff4ae51c641edd963ffe3b3d3b6893d94
Mihai <mihai.giosan.2020@gmail.com>
2021-08-14 16:57:19 +0300

    Add map

---
+++ src/main/resources/static/script/map.js
@@ -0,0 +1,24 @@
+	var mymap = L.map('mapid').setView([44.426057, 26.104288], 13);
+
+	L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+		maxZoom: 18,
+		attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, ' +
+			'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+		id: 'mapbox/streets-v11',
+		tileSize: 512,
+		zoomOffset: -1
+	}).addTo(mymap);
+
+	var popup = L.popup();
+
+    function onMapClick(e) {
+        popup
+            .setLatLng(e.latlng)
+            .setContent("You clicked the map at " + e.latlng.toString())
+            .openOn(mymap);
+    }
+
+    mymap.on('click', onMapClick);
+
+    var markerHome = L.marker([44.453044, 26.124582]).addTo(mymap);
+    markerHome.bindPopup("<b>Hello world!</b><br>This is my home.");
